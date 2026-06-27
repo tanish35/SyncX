7808cfe8586f47c6c2fc509168598a7028145b30
tanish35 <tanishmajumdar2912@gmail.com>
2026-06-27 16:40:15 +0530

    chore: add ESLint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+});
+
+const eslintConfig = [
+  {
+    ignores: [".next/**", ".open-next/**", "node_modules/**", "next-env.d.ts"],
+  },
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
