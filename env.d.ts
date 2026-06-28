f5c9e7ffd88c5b2c49f7295db3ac9985c0608c75
tanish35 <tanishmajumdar2912@gmail.com>
2026-06-28 21:34:40 +0530

    chore: add Cloudflare environment type definitions

---
+++ env.d.ts
@@ -0,0 +1,8 @@
+interface CloudflareEnv {
+  DB: D1Database;
+  SESSION_SECRET: string;
+  CRED_ENC_KEY: string;
+  TMDB_ACCESS_TOKEN: string;
+  GMAIL_USER: string;
+  GMAIL_APP_PASSWORD: string;
+}
